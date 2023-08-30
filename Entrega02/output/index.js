29b9d1c81dfcc0c936984acd226840430374cca1
Caputino <10758372+capuano1@users.noreply.github.com>
2023-08-30 20:28:17 -0300

    Entrega 02
    Entrega 02

---
+++ Entrega02/output/index.js
@@ -0,0 +1,41 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const bike_1 = require("./bike");
+const user_1 = require("./user");
+const app_1 = require("./app");
+const app = new app_1.App;
+const mbike = new bike_1.Bike('mountain bike', 'mountain', 123, 500, 100.5, 'desc', 5, []);
+const maria = new user_1.User('Maria', 'maria@mail.com', '1234');
+const sbike = new bike_1.Bike('speed bike', 'speed', 456, 250, 90.5, 'desc', 4, []);
+const joao = new user_1.User('João', 'joao@mail.com', '5678');
+//Quando criamos uma Date no js, ele coloca como a data atual
+const today = new Date();
+const twoDaysFromToday = new Date();
+twoDaysFromToday.setDate(twoDaysFromToday.getDate() + 2);
+const tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+const sevenDaysFromToday = new Date();
+sevenDaysFromToday.setDate(sevenDaysFromToday.getDate() + 7);
+const sevenDaysAgo = new Date();
+sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+const twoDaysAgo = new Date();
+twoDaysAgo.setDate(twoDaysAgo.getDate() - 2);
+//const rent1 = Rent.create([], mbike, maria, today, twoDaysFromToday)
+//const rent2 = Rent.create([rent1], bike, user, tomorrow, sevenDaysFromToday)
+//Como ele vai dar uma exceção, por conta da bike não estar disponível, ele para o código e dá erro
+//console.log(rent2)
+app.addUser(maria);
+app.addUser(joao);
+app.addBike(mbike);
+app.addBike(sbike);
+console.log(app.users);
+console.log(app.bikes);
+app.rentBike(app.bikes[app.bikes.findIndex(rBike => { return rBike.name === 'speed bike'; })].id, 'joao@mail.com', sevenDaysAgo, twoDaysAgo);
+console.log(app.rents);
+console.log("FINALIZADO 1");
+app.removeUser('maria@mail.com');
+app.removeBike(app.bikes[app.bikes.findIndex(rBike => { return rBike.name === 'mountain bike'; })].id);
+app.returnBike('joao@mail.com', app.bikes[app.bikes.findIndex(rBike => { return rBike.name === 'speed bike'; })].id);
+console.log(app.users);
+console.log(app.bikes);
+console.log(app.rents);
---
+++ Entrega02/output/modelo.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Modelo = void 0;
+class Modelo {
+    constructor(marca, nome, numPortas, potencia, combustivel) {
+        this.marca = marca;
+        this.nome = nome;
+        this.numPortas = numPortas;
+        this.potencia = potencia;
+        this.combustivel = combustivel;
+    }
+}
+exports.Modelo = Modelo;
