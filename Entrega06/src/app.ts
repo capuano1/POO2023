b780a081eb3164fa95c30140288735ec11c683ae
Caputino <10758372+capuano1@users.noreply.github.com>
2023-09-18 20:42:04 -0300

    Update app.ts

--- Entrega06/src/app.ts
+++ Entrega06/src/app.ts
@@ -96,7 +96,7 @@ export class App {
 
     moveBikeTo(bikeId: string, location: Location) {
         const bike = this.bikes.find(bike => bike.id === bikeId)
-        if (bike == undefined) throw new Error("Não existe essa bicicleta seu idiota")
+        if (bike == undefined) throw new Error("Bicicleta não encontrada")
         bike.location.latitude = location.latitude
         bike.location.longitude = location.longitude
     }
